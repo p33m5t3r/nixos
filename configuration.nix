f60a956098c34158b262f889cfb477fc8bfaa358
p33m5t3r <bcl4rk@protonmail.com>
2025-02-08 18:43:09 -0700

    fixed audio

--- configuration.nix
+++ configuration.nix
@@ -4,23 +4,21 @@
 { config, lib, pkgs, ... }:
 
 { imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix ];
+    [ ./hardware-configuration.nix ];
 
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true; boot.loader.efi.canTouchEfiVariables = true;
+  # boot
+  boot.loader.systemd-boot.enable = true; 
+  boot.loader.efi.canTouchEfiVariables = true;
 
-  # networking.hostName = "nixos"; # Define your hostname. Pick only one of the below networking options. networking.wireless.enable = true; # Enables 
-  # wireless support via wpa_supplicant. networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
+  # networking
   networking = {
     hostName = "nixos";
     networkmanager.enable = true;
   };
-  # Set your time zone. 
+
+  # time zone. 
   time.timeZone = "America/Denver";
 
-  # Configure network proxy if necessary networking.proxy.default = "http://user:password@proxy:port/"; networking.proxy.noProxy = 
-  # "127.0.0.1,localhost,internal.domain";
 
   # Select internationalisation properties. i18n.defaultLocale = "en_US.UTF-8"; console = {
   #   font = "Lat2-Terminus16"; keyMap = "us"; useXkbConfig = true; # use xkb.options in tty.
@@ -45,14 +43,16 @@
     ];
   };
 
-  # sound stuff
+  # sound stuff: use pactl for volume control
+  hardware.pulseaudio.enable=true;
+  services.pipewire.enable=false;
   security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-  };
+  # services.pipewire = {
+  #   enable = true;
+  #   pulse.enable = true;
+  #   alsa.enable = true;
+  #   alsa.support32Bit = true;
+  # };
   
 
   # Configure keymap in X11 services.xserver.xkb.layout = "us"; services.xserver.xkb.options = "eurosign:e,caps:escape";
@@ -72,13 +72,10 @@
   # };
   users.users.anon = {
     isNormalUser = true;
-    extraGroups  = ["wheel" "networkmanager"];
+    extraGroups  = ["wheel" "networkmanager" "audio"];
     initialPassword = "anon";
   };
 
-
-  # programs.firefox.enable = true;
-  
   # List packages installed in system profile. To search, run: $ nix search wget environment.systemPackages = with pkgs; [
   #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default. wget
   # ];
@@ -87,6 +84,11 @@
     vim
     wget
     git
+    firefox
+
+    pavucontrol pamixer
+    psmisc # killall
+    htop
   ];
 
   # Some programs need SUID wrappers, can be configured further or are started in user sessions. programs.mtr.enable = true; programs.gnupg.agent = {
@@ -101,25 +103,8 @@
   # Open ports in the firewall. networking.firewall.allowedTCPPorts = [ ... ]; networking.firewall.allowedUDPPorts = [ ... ]; Or disable the firewall 
   # altogether. networking.firewall.enable = false;
 
-  # Copy the NixOS configuration file and link it from the resulting system (/run/current-system/configuration.nix). This is useful in case you 
-  # accidentally delete configuration.nix. 
+  # NO TOUCH!
   system.copySystemConfiguration = true;
-
-  # This option defines the first version of NixOS you have installed on this particular machine, and is used to maintain compatibility with 
-  # application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason, even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from, so changing it will NOT upgrade your system - see 
-  # https://nixos.org/manual/nixos/stable/#sec-upgrading for how to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration, and migrated your data 
-  # accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "24.11"; # Did you read the comment?
-
+  system.stateVersion = "24.11";
 }
 
